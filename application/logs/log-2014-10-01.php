58e6cd69fbf066e9a1ccf086388a344e363f7427
Saiful Habib <sangpejuang32@gmail.com>
2014-10-10 09:45:33 +0700

    Initial Commit for setting up the Code Igniter

---
+++ application/logs/log-2014-10-01.php
@@ -0,0 +1,218 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-10-01 12:33:08 --> Config Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:33:08 --> URI Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Router Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> No URI present. Default controller set.
+DEBUG - 2014-10-01 12:33:08 --> Output Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Security Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Input Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:33:08 --> Language Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:33:08 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:33:08 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:33:08 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:33:08 --> Session Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:33:08 --> A session cookie was not found.
+DEBUG - 2014-10-01 12:33:08 --> Session routines successfully run
+DEBUG - 2014-10-01 12:33:08 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:33:08 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-10-01 12:33:08 --> Final output sent to browser
+DEBUG - 2014-10-01 12:33:08 --> Total execution time: 0.8425
+DEBUG - 2014-10-01 12:33:32 --> Config Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:33:32 --> URI Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Router Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> No URI present. Default controller set.
+DEBUG - 2014-10-01 12:33:32 --> Output Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Security Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Input Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:33:32 --> Language Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:33:32 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:33:32 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:33:32 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:33:32 --> Session Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:33:32 --> Session routines successfully run
+DEBUG - 2014-10-01 12:33:32 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:33:32 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-10-01 12:33:32 --> Final output sent to browser
+DEBUG - 2014-10-01 12:33:32 --> Total execution time: 0.0255
+DEBUG - 2014-10-01 12:33:46 --> Config Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:33:46 --> URI Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Router Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Output Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Security Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Input Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:33:46 --> Language Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:33:46 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:33:46 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:33:46 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:33:46 --> Session Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:33:46 --> Session routines successfully run
+DEBUG - 2014-10-01 12:33:46 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:33:46 --> File loaded: application/views/test.php
+DEBUG - 2014-10-01 12:33:46 --> Final output sent to browser
+DEBUG - 2014-10-01 12:33:46 --> Total execution time: 0.0239
+DEBUG - 2014-10-01 12:36:01 --> Config Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:36:01 --> URI Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Router Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> No URI present. Default controller set.
+DEBUG - 2014-10-01 12:36:01 --> Output Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Security Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Input Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:36:01 --> Language Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:36:01 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:36:01 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:36:01 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:36:01 --> Session Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:36:01 --> Session routines successfully run
+DEBUG - 2014-10-01 12:36:01 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:36:01 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-10-01 12:36:01 --> Final output sent to browser
+DEBUG - 2014-10-01 12:36:01 --> Total execution time: 0.0233
+DEBUG - 2014-10-01 12:38:05 --> Config Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:38:05 --> URI Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Router Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> No URI present. Default controller set.
+DEBUG - 2014-10-01 12:38:05 --> Output Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Security Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Input Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:38:05 --> Language Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:38:05 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:38:05 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:38:05 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:38:05 --> Session Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:38:05 --> Session routines successfully run
+DEBUG - 2014-10-01 12:38:05 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:38:05 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-10-01 12:38:05 --> Final output sent to browser
+DEBUG - 2014-10-01 12:38:05 --> Total execution time: 0.0116
+DEBUG - 2014-10-01 12:38:09 --> Config Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:38:09 --> URI Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Router Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Output Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Security Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Input Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:38:09 --> Language Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:38:09 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:38:09 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:38:09 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:38:09 --> Session Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:38:09 --> Session routines successfully run
+DEBUG - 2014-10-01 12:38:09 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:38:09 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-10-01 12:38:09 --> Final output sent to browser
+DEBUG - 2014-10-01 12:38:09 --> Total execution time: 0.0137
+DEBUG - 2014-10-01 12:38:14 --> Config Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:38:14 --> URI Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Router Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Output Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Security Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Input Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:38:14 --> Language Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:38:14 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:38:14 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:38:14 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:38:14 --> Session Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:38:14 --> Session routines successfully run
+DEBUG - 2014-10-01 12:38:14 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:38:14 --> Controller Class Initialized
+ERROR - 2014-10-01 12:38:14 --> 404 Page Not Found --> welcome/tes
+DEBUG - 2014-10-01 12:38:28 --> Config Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Hooks Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 12:38:28 --> URI Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Router Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Output Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Security Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Input Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-01 12:38:28 --> Language Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Loader Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Helper loaded: url_helper
+DEBUG - 2014-10-01 12:38:28 --> Helper loaded: file_helper
+DEBUG - 2014-10-01 12:38:28 --> Database Driver Class Initialized
+ERROR - 2014-10-01 12:38:28 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /var/www/html/SIPERPU-CI/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2014-10-01 12:38:28 --> Session Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Helper loaded: string_helper
+DEBUG - 2014-10-01 12:38:28 --> Session routines successfully run
+DEBUG - 2014-10-01 12:38:28 --> Pagination Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> Controller Class Initialized
+DEBUG - 2014-10-01 12:38:28 --> File loaded: application/views/test.php
+DEBUG - 2014-10-01 12:38:28 --> Final output sent to browser
+DEBUG - 2014-10-01 12:38:28 --> Total execution time: 0.0236
+DEBUG - 2014-10-01 21:01:39 --> Config Class Initialized
+DEBUG - 2014-10-01 21:01:39 --> Hooks Class Initialized
+DEBUG - 2014-10-01 21:01:39 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 21:01:39 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 21:01:39 --> URI Class Initialized
+DEBUG - 2014-10-01 21:01:39 --> Router Class Initialized
+ERROR - 2014-10-01 21:01:39 --> 404 Page Not Found --> Tutorial-CI
+DEBUG - 2014-10-01 21:01:42 --> Config Class Initialized
+DEBUG - 2014-10-01 21:01:42 --> Hooks Class Initialized
+DEBUG - 2014-10-01 21:01:42 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 21:01:42 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 21:01:42 --> URI Class Initialized
+DEBUG - 2014-10-01 21:01:42 --> Router Class Initialized
+ERROR - 2014-10-01 21:01:42 --> 404 Page Not Found --> Tutorial-CI
+DEBUG - 2014-10-01 21:01:45 --> Config Class Initialized
+DEBUG - 2014-10-01 21:01:45 --> Hooks Class Initialized
+DEBUG - 2014-10-01 21:01:45 --> Utf8 Class Initialized
+DEBUG - 2014-10-01 21:01:45 --> UTF-8 Support Enabled
+DEBUG - 2014-10-01 21:01:45 --> URI Class Initialized
+DEBUG - 2014-10-01 21:01:45 --> Router Class Initialized
+ERROR - 2014-10-01 21:01:45 --> 404 Page Not Found --> Tutorial-CI
